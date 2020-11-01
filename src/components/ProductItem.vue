bfbc7260eb31b989dc45228e7daa4cbe10c52309
Andrew <dummy@example.local>
2020-11-01 14:43:46 +0300

    Lesson 4 (add)

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -15,24 +15,10 @@
     </span>
 
     <ul class="colors colors--black">
-      <li class="colors__item">
+      <li class="colors__item" v-for="colorVal in product.colors" :key="colorVal">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" value="#73B6EA" v-model="color">
-          <span class="colors__value" style="background-color: #73B6EA;">
-          </span>
-        </label>
-      </li>
-      <li class="colors__item">
-        <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" value="#8BE000" v-model="color">
-          <span class="colors__value" style="background-color: #8BE000;">
-          </span>
-        </label>
-      </li>
-      <li class="colors__item">
-        <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" value="#222" v-model="color">
-          <span class="colors__value" style="background-color: #222;">
+          <input class="colors__radio sr-only" type="radio" :value="colorVal" v-model="color">
+          <span class="colors__value" :style="{backgroundColor: colorVal}">
           </span>
         </label>
       </li>
